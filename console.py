ef65290329d7bb7cf3b31e493a4f5a5e3d5479e2
Mardie328 <tolayinka328@gmail.com>
2023-10-15 16:05:35 +0100

    feat: add a file that controls the entry point into the program

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Program that controls the entry point into the command-line."""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class that handles the command-line interface."""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Exit the program"""
+        return(True)
+    
+    def help_quit(self):
+        print("Quit command to exit the program")
+    
+    def do_EOF(self, line):
+        """Exit the command-line interface with EOF(ctrl+D)"""
+        print()
+        return(True)
+    
+        
+
+
+if __name__=="__main__":
+    HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,6 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """
             constructor for all models with the given arguments and kwargs
-            Args: None
-            Returns: None
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
