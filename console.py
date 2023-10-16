d139c46612c44f6051553772ae493df57ac33691
Mardie328 <tolayinka328@gmail.com>
2023-10-16 02:14:52 +0100

    updated

--- console.py
+++ console.py
@@ -4,20 +4,56 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Class that handles the command-line interface."""
-    prompt = "(hbnb) "
+    """ HBNH console """
+    prompt = '(hbnb) '
 
-    def do_quit(self, line):
-        """Exit the program"""
-        return(True)
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
 
-    def help_quit(self):
-        print("Quit command to exit the program")
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
 
-    def do_EOF(self, line):
-        """Exit the command-line interface with EOF(ctrl+D)"""
-        print()
-        return(True)
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def _key_value_parser(self, args):
+        """creates a dictionary from a list of strings"""
+        new_dict = {}
+        for arg in args:
+            if "=" in arg:
+                kvp = arg.split('=', 1)
+                key = kvp[0]
+                value = kvp[1]
+                if value[0] == value[-1] == '"':
+                    value = shlex.split(value)[0].replace('_', ' ')
+                else:
+                    try:
+                        value = int(value)
+                    except:
+                        try:
+                            value = float(value)
+                        except:
+                            continue
+                new_dict[key] = value
+        return new_dict
+
+    def do_create(self, arg):
+        """Creates a new instance of a class"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            new_dict = self._key_value_parser(args[1:])
+            instance = classes[args[0]](**new_dict)
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(instance.id)
+        instance.save()
 
 
 if __name__ == "__main__":
